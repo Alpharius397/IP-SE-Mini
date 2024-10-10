f9355d3e192cb11442ffda27c3c7f764fae328f8
Alpharius397 <rajjm397@gmail.com>
2024-10-10 09:55:31 +0530

    done?

--- mini/admin.py
+++ mini/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Donor,Bank,Admin,Camp
+from .models import Donor,Bank,Admin,Camp,CampReg
 
 admin.site.register(Donor)
 admin.site.register(Admin)
 admin.site.register(Bank)
 admin.site.register(Camp)
+admin.site.register(CampReg)
 
